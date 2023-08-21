f1e2792e63e9150a27389c8190c7fa965ff7d851
KooshaOrazizade <79403471+xawolf@users.noreply.github.com>
2023-08-21 23:38:36 +0330

    Update zfs.sh

--- ansible/zfs.sh
+++ ansible/zfs.sh
@@ -4,6 +4,6 @@ sudo pvcreate /dev/sdb1
 sleep 1
 sudo vgcreate zfs-vg /dev/sdb1
 sleep 1
-sudo lvcreate -L 198G zfs-vg -n zfs-lv
+sudo lvcreate -l +100%FREE /dev/zfs-vg -n zfs-lv
 sleep 1
 sudo zpool create zfs-pool  /dev/zfs-vg/zfs-lv
